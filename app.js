01cb6d9b66be1bf3e3454229f0db125f7c1b3a6e
SaaGii <sandagdorj74@gmail.com>
2023-12-20 12:53:53 +0800

    app.js dice button hiiw

--- app.js
+++ app.js
@@ -9,23 +9,18 @@ var scores = [0, 0];
 var roundScore = 0;
 
  // Shoonii ali talaaraa buusniig hadgalah huwisagch heregtei, 1-6 gesen utgiig ene huwisagchid sanamsarguigeer uusgej ogno
- var dice = Math.floor(Math.random() * 6) + 1;
-//  var dice1 = Math.floor(Math.random() * 6) + 1;
-
-// <div class="player-score" id="score-0">43</div>
-
-// window.document.querySelector('#score-0').textContent = dice;
-// document.querySelector('#score-1').textContent = dice1;
-// document.querySelector('#score-1').innerHTML = "<em>Yes!<em>";
 
 //Program ehlehed beltgey
+document.getElementById('score-1').textContent = "0";
+document.getElementById('score-0').textContent = "0";
+document.getElementById('current-0').textContent = "0";
+document.getElementById('current-1').textContent = "0";
 
-document.querySelector('#score-0').textContent = 0;
-document.querySelector('#score-1').textContent = 0;
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
 
-document.querySelector('#current-0').textContent = 0;
-document.querySelector('#current-1').textContent = 0;
-
-document.querySelector(".dice").style.display = 'none';
-
-console.log('Shoo: ' + dice);
+document.querySelector(".btn-roll").addEventListener("click", function(){
+    var diceNumber= Math.floor(Math.random() * 6) + 1;
+    diceDom.style.display = "block";
+    diceDom.src = "dice-" + diceNumber + ".png";
+});
