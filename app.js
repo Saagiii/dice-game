1c86523b5d52dfb66fab1b73f416775a58ac85a6
SaaGii <sandagdorj74@gmail.com>
2023-12-22 10:48:05 +0800

    Ura, togloomoo hiij duuslaa.

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
-// Toglogchiin eeljiig hadgalah huwisagch, negdugeer toglogchiig 0, hoerdugaar toglogchiig 1 gej temdegley.
-var activePlayer = 0;
+// Togloomiin buh gazart ashiglagdah global huwisagchdiig end zarlay
 
+// Togloom duussan esehiig hadgalah tolowiin huwisagch
+var isNewGame;
+
+// Ali toglogch shoo shideh we gedgiig end hadgalna
+var activePlayer;
+
+// Hoer toglogchiin tsugluulsan onoo
+var scores;
+
+// Idehtei toglogchiin tsugluul baigaa eeljiin onoo
+var roundScore;
+
+// Shoonii zurgiig uzuuleh elementiig DOM-s haij olood end hadgalya.
+var diceDom = document.querySelector(".dice");
+
+// Togloomiig ehluulne.
+initGame();
+
+// Togloogmiig shineer ehlehed beltgene.
+function initGame(){
+// Togloom ehellee gedeg tolowt oruulna.
+isNewGame = true;
+
+// Toglogchiin eeljiig hadgalah huwisagch, negdugeer toglogchiig 0, hoerdugaar toglogchiig 1 gej temdegley.
+activePlayer = 0;
 
 // Toglogchdiin tsugluulsan onoog hadgalah huwisagch
-var scores = [0, 0];
+scores = [0, 0];
 
  // Toglogchiin eeljindee tsugluulj baigaa onoog hadgalah huwisagch
-var roundScore = 0;
-
- // Shoonii ali talaaraa buusniig hadgalah huwisagch heregtei, 1-6 gesen utgiig ene huwisagchid sanamsarguigeer uusgej ogno
+roundScore = 0;
 
 //Program ehlehed beltgey
 document.getElementById('score-0').textContent = "0";
@@ -16,14 +38,25 @@ document.getElementById('score-1').textContent = "0";
 document.getElementById('current-0').textContent = "0";
 document.getElementById('current-1').textContent = "0";
 
-var diceDom = document.querySelector(".dice");
-diceDom.style.display = "none";
+// Toglogchdiin neriig butsaaj gargah
+document.getElementById('name-0').textContent = 'Player 1';
+document.getElementById('name-1').textContent = 'Player 2';
 
+document.querySelector('.player-0-panel').classList.remove('winner');
+document.querySelector('.player-1-panel').classList.remove('winner');
+
+document.querySelector('.player-0-panel').classList.remove('active');
+document.querySelector('.player-1-panel').classList.remove('active');
+
+document.querySelector('.player-0-panel').classList.add('active');
+
+diceDom.style.display = "none";
+}
 
 // Shoog shideh event listener
 document.querySelector(".btn-roll").addEventListener("click", function(){
-
- // 1-6 dotor sanamsargui toog gargaj awna
+   if(isNewGame){      // isNewGame === true
+    // 1-6 dotor sanamsargui toog gargaj awna
     var diceNumber= Math.floor(Math.random() * 6) + 1;
 
  // Shoonii zurgiig web deer gargaj irne
@@ -41,18 +74,24 @@ document.querySelector(".btn-roll").addEventListener("click", function(){
         // 1 buusan tul toglogchiin eeljiig ene hesegt solij ugnu.
         switchToNextPlayer();
  }
+   }else{
+    alert("Тоглоом дууссан байна. NEW GAME товчийг дарж шинээр эхлэнэ үү!");
+   }
 });
 // Hold towchnii event listener
 document.querySelector('.btn-hold').addEventListener('click', function(){
-    // Ug toglogchiin tsugluulsan eeljnii onoog global onoon deer n nemj ogno
+    if(isNewGame){
+        // Ug toglogchiin tsugluulsan eeljnii onoog global onoon deer n nemj ogno
     scores[activePlayer] = scores[activePlayer] + roundScore;
 
     // Delgets deer onoog ni uurchlunu.
 document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
 
-
     // Ug toglogch hojson esehiig shalgah
-    if(scores[activePlayer] >= 10){
+    if(scores[activePlayer] >= 100){
+        // Togloomiig duussan tolowt oruulna.
+        isNewGame = false;
+
         // Winner gesen textiig nerniih ni orond gargana.
         document.getElementById('name-' + activePlayer).textContent = "Winner!";
         
@@ -65,7 +104,9 @@ document.getElementById('score-' + activePlayer).textContent = scores[activePlay
         // Toglogchiin eeljiig solino.
         switchToNextPlayer()
     }
-
+    }else{
+        alert("Тоглоом дууссан байна. NEW GAME товчийг дарж шинээр эхлэнэ үү!");
+    }
 });
 
 // Ene function ni togloh eeljiig daraachiin toglogchruu shiljuuldeg.
@@ -86,6 +127,8 @@ function switchToNextPlayer(){
     // }else{
     //     activePlayer = 0;
     // }
-// Shoog tur alga bolgon.
+// Shoog tur alga bolgono.
 diceDom.style.display = "none";
 }
+// New game buyu Shine togloom ehluuleh towchnii event listener
+document.querySelector('.btn-new').addEventListener('click', initGame);
