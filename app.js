c22025d7c39461b1b162b62107e03af67a783caf
SaaGii <106458176+Saagiii@users.noreply.github.com>
2023-12-25 20:19:17 +0800

    Update app.js

--- app.js
+++ app.js
@@ -39,8 +39,8 @@ document.getElementById('current-0').textContent = "0";
 document.getElementById('current-1').textContent = "0";
 
 // Toglogchdiin neriig butsaaj gargah
-document.getElementById('name-0').textContent = 'Yuki';  // Player 1
-document.getElementById('name-1').textContent = 'SaaGii';
+document.getElementById('name-0').textContent = 'сука1: Учрал';  // Player 1
+document.getElementById('name-1').textContent = 'сука2: Билгүүн';
 
 document.querySelector('.player-0-panel').classList.remove('winner');
 document.querySelector('.player-1-panel').classList.remove('winner');
@@ -75,7 +75,7 @@ document.querySelector(".btn-roll").addEventListener("click", function(){
         switchToNextPlayer();
  }
    }else{
-    alert("Тоглоом дууссан байна. NEW GAME товчийг дарж шинээр эхлэнэ үү бондоок оо!");
+    alert("Тоглоом дууссан байна. NEW GAME товчийг дарж шинээр эхлээрэй 2 сда!");
    }
 });
 // Hold towchnii event listener
@@ -105,7 +105,7 @@ document.getElementById('score-' + activePlayer).textContent = scores[activePlay
         switchToNextPlayer()
     }
     }else{
-        alert("Тоглоом дууссан байна. NEW GAME товчийг дарж шинээр эхлэнэ үү бондоок оо!");
+        alert("Тоглоом дууссан байна. NEW GAME товчийг дарж шинээр эхлээрэй 2 сда!");
     }
 });
 
